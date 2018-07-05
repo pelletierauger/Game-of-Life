19a94efb6c3739b7ad7ace67d2328728505374d9
Guillaume Pelletier-Auger <pelletierauger@gmail.com>
2018-07-05 14:07:38 -0400

    Work more on the high definition scenes

--- scenes-to-print.js
+++ scenes-to-print.js
@@ -245,8 +245,8 @@ let sunset01 = new Scene({
     fileName: "./frames/inner-january-14/inner-january-14",
     gridScalar: 16,
     horizontalScalar: 16,
-    verticalScalar: 11,
-    fixedGridSize: { width: 257, height: 176 },
+    verticalScalar: 12,
+    fixedGridSize: { width: 257, height: 179 },
     // offset: { x: 500, y: 500 },
     // fixedGridSize: { width: 1000, height: 1000 },
     // paletteName: "palette-fri-dec-15-2017-185009",
@@ -309,7 +309,7 @@ let sunset01 = new Scene({
     paletteName: "palette-sun-dec-24-2017-003950",
 
     // Classic The Dig
-    paletteName: "palette-mon-dec-11-2017-211645",
+    // paletteName: "palette-mon-dec-11-2017-211645",
 
     // paletteName: "palette-mon-dec-18-2017-005144",
 
@@ -393,7 +393,7 @@ sunset01.updateGrid = function() {
     //     }
     // }
     if (this.currentState % 10 == 0 || this.currentState == 1) {
-        let yBase = map(this.currentState, 0, 125, this.gridYAmount, 0);
+        let yBase = map(this.currentState, 0, 125, this.gridYAmount - 25, 0);
         let ran = Math.random() * 50;
         let increment = Math.random() * 20;
         for (var x = this.gridXAmount * 0.5; x < this.gridXAmount; x += increment) {
@@ -1443,8 +1443,227 @@ nightsOfMarch33b.getColor = function(oneDValue, optionalArray) {
     a.g = lerp(a.g, 0, thirdLerp);
     a.b = lerp(a.b, 0, thirdLerp);
     a = adjustLevels(0, 60, 150, { r: a.r, g: a.g, b: a.b });
-    return color(a.r, a.g, a.b);
+    return color(255, 255, 255);
 };
 
+//-------------------------------------------------------------
 
-scene = nightsOfMarch33b;
+let nightsOfMarch22c = new Scene({
+    fileName: "./frames/nights-of-march-22-levels/nights-of-march-22",
+    gridScalar: 16,
+    // offset: { x: 500, y: 500 },
+    // fixedGridSize: { width: 1000, height: 1000 },
+    // paletteName: "palette-fri-dec-15-2017-185009",
+    // paletteName: "palette-tue-dec-19-2017-173106",
+
+    horizontalScalar: 16,
+    verticalScalar: 11,
+    fixedGridSize: { width: 257, height: 173 },
+
+    // beau
+    // paletteName: "palette-sat-dec-16-2017-150022",
+    // très beau
+    paletteName: "palette-tue-dec-12-2017-141143",
+    // aussi très beau
+    // paletteName: "palette-sat-mar-17-2018-153848",
+
+
+    // Beau avec nightsOfMarch19
+    paletteName: "palette-sun-mar-18-2018-205032",
+
+
+    paletteName: "palette-sun-dec-24-2017-140250",
+
+    paletteName: "palette-tue-dec-12-2017-132118",
+
+    paletteName: "palette-tue-dec-12-2017-221300",
+
+
+    paletteName: "palette-thu-jan-11-2018-172555",
+    // nightsOfMarch22 aussi très beau avec :
+    // palette-sat-dec-16-2017-142619
+    // palette-tue-dec-19-2017-172500
+    // palette-tue-dec-12-2017-220058
+
+    //----------------------------------------
+
+    // Other nice options for nightsOfMarch19:
+    // palette-mon-dec-11-2017-211545
+    // palette-thu-jan-11-2018-172555
+    // palette-sat-mar-17-2018-152553
+    // palette-mon-mar-19-2018-224024
+    // palette-mon-mar-19-2018-224330
+
+    // Wonderful for nightsOfMarch19:
+    // palette-mon-mar-19-2018-224636
+
+    //palette-mon-mar-19-2018-225739
+    //palette-mon-mar-19-2018-231044
+    //palette-tue-mar-20-2018-013327
+    //palette-tue-mar-20-2018-015216
+
+    //----------------------------------------
+
+    // paletteName: "palette-sun-mar-04-2018-154033",
+    // palette-tue-dec-12-2017-141143
+
+    speedModulo: 1,
+    zoom: 1,
+    dotPerTile: 3500 / 16 * 1.8,
+    maxSteps: 129
+});
+
+nightsOfMarch22c.applyShapes = function() {
+    // this.setGridValue(this.gridXAmount / 2, this.gridYAmount / 2, 1);
+    // this.setGridValue(this.gridXAmount / 2, 0, 1);
+    // this.setGridValue(0, this.gridYAmount / 2, 1);
+    // this.setGridValue(this.gridXAmount * 0.22, this.gridYAmount * 0.12, 1);
+    // this.setGridValue(this.gridXAmount * 0.8, this.gridYAmount * 0.5, 1);
+
+};
+
+nightsOfMarch22c.updateGrid = function() {
+
+    // if (Math.random() <= 0.9) {
+    for (let u = 0; u < 10; u++) {
+        let x = Math.random() * this.gridXAmount;
+        let y = Math.random() * this.gridYAmount;
+        let w = Math.random() * 10;
+        let yModifier = 0;
+        let modifier = (Math.random() >= 0.5) ? true : false;
+        for (let i = 0; i < w; i++) {
+            let plusMinus = (Math.random() >= 0.5) ? -1 : 1;
+            // if (modifier) {
+            yModifier += plusMinus;
+            // }
+            let xAmount = (this.fixedGridSize) ? this.fixedGridSize.width : this.gridXAmount;
+            let oneDValue = (Math.floor(x) + i) + ((Math.floor(y) + yModifier) * xAmount);
+            if (this.changes[oneDValue] == 0) {
+                this.setGridValue(x + i, y + yModifier, 1);
+                this.changes[oneDValue] = this.currentState * 0.5;
+            }
+        }
+    }
+    // }
+    // if (Math.random() <= 0.5) {
+    //     let x = map(this.currentState, 0, 100, this.gridXAmount, 0);
+    //     let y = Math.random() * this.gridYAmount;
+    //     let w = Math.random() * 40;
+    //     let yModifier = 0;
+    //     let modifier = (Math.random() >= 0.5) ? true : false;
+    //     for (let i = 0; i < w; i++) {
+    //         let plusMinus = (Math.random() >= 0.5) ? -1 : 1;
+    //         // if (modifier) {
+    //         yModifier += plusMinus;
+    //         // }
+    //         let xAmount = (this.fixedGridSize) ? this.fixedGridSize.width : this.gridXAmount;
+    //         let oneDValue = (Math.floor(x) + i) + ((Math.floor(y) + yModifier) * xAmount);
+    //         if (this.changes[oneDValue] == 0) {
+    //             this.setGridValue(x + i, y + yModifier, 1);
+    //             this.changes[oneDValue] = this.currentState * 0.5;
+    //         }
+    //     }
+    // }
+    let xAmount = (this.fixedGridSize) ? this.fixedGridSize.width : this.gridXAmount;
+    let yAmount = (this.fixedGridSize) ? this.fixedGridSize.height : this.gridYAmount;
+    for (var x = 0; x < xAmount; x++) {
+        for (var y = 0; y < yAmount; y++) {
+            var oneDValue = x + (y * xAmount);
+            var value = this.grid[oneDValue].state;
+            var neighbors = this.calculateNeighbors(x, y);
+            var neighborTopLeft = this.calculateNeighbors(x - 1, y - 1);
+            var neighborTop = this.calculateNeighbors(x, y - 1);
+            var neighborTopRight = this.calculateNeighbors(x + 1, y - 1);
+            var neighborRight = this.calculateNeighbors(x + 1, y);
+            var neighborBottomRight = this.calculateNeighbors(x + 1, y + 1);
+            var neighborBottom = this.calculateNeighbors(x, y + 1);
+            var neighborBottomLeft = this.calculateNeighbors(x - 1, y + 1);
+            var neighborLeft = this.calculateNeighbors(x - 1, y);
+            let changed = false;
+            if (value == 1) {
+                if (neighbors == 1 || neighbors == 2 || neighbors == 3 || neighbors == 0) {
+                    this.next[oneDValue] = { state: 0, changed: true };
+                    this.changes[oneDValue] = this.currentState * 0.5;
+                    changed = true;
+                }
+            } else {
+                if (neighborBottom <= 2 && neighborTop) {
+                    this.next[oneDValue] = { state: 1, changed: true };
+                    this.changes[oneDValue] = this.currentState * 0.5;
+                    changed = true;
+                }
+            }
+            if (!changed) {
+                this.next[oneDValue] = { state: value, changed: false };
+            }
+        }
+    }
+    for (var i = 0; i < this.grid.length; i++) {
+        this.grid[i] = this.next[i];
+    }
+    this.currentState++;
+};
+
+nightsOfMarch22c.calculateNeighbors = function(x, y) {
+    var sum = 0;
+    sum += this.getGridValue(x - 1, y - 1);
+    sum += this.getGridValue(x, y - 1);
+    sum += this.getGridValue(x + 1, y - 1);
+    sum += this.getGridValue(x - 1, y);
+    sum += this.getGridValue(x + 1, y);
+    sum += this.getGridValue(x - 1, y + 1);
+    sum += this.getGridValue(x, y + 1);
+    sum += this.getGridValue(x + 1, y + 1);
+    return sum;
+};
+nightsOfMarch22c.update = function() {
+    // console.log("UPDATED!!!!");
+    // this.palette.data.redOsc *= 0.9;
+    // this.palette.data.greenOsc *= 0.9;
+    // this.palette.data.blueOsc *= 0.9;
+    // if (this.currentState % 2 == 0) {
+    //     this.updateGrid = biggestFractal.updateGrid;
+    // } else {
+    //     this.updateGrid = beforeTheRiverFractal10.updateGrid;
+    // }
+    if (!exporting && this.currentState == 0) {
+        this.currentState++;
+    } else {
+        if (!printing) {
+            this.updateGrid();
+        } else if (printing) {
+            if (this.counter % this.speedModulo == 0) {
+                this.updateGrid();
+            }
+            this.counter++;
+        }
+    }
+};
+nightsOfMarch22c.getColor = function(oneDValue, optionalArray) {
+    let c;
+    if (optionalArray) {
+        c = optionalArray[oneDValue];
+    } else {
+        c = this.changes[oneDValue];
+    }
+    let blueLerp = map(c, 0, 50, 0, 1);
+    blueLerp = constrain(blueLerp, 0, 1);
+    let blackLerp = map(c, 50, 150, 0, 1);
+    blackLerp = constrain(blackLerp, 0, 1);
+    let p = this.palette.data;
+    let red = map(sin(c / p.redOsc), -1, 1, p.redMin, p.redMax);
+    let green = map(sin(c / p.greenOsc), -1, 1, p.greenMin, p.greenMax);
+    let blue = map(sin(c / p.blueOsc), 1, -1, p.blueMin, p.blueMax);
+    // let a = adjustLevels(0, 0, 150, { r: red, g: green, b: blue });
+    let a = { r: red, g: green, b: blue };
+    a.r = lerp(a.r, 75, blueLerp);
+    a.g = lerp(a.g, 0, blueLerp);
+    a.b = lerp(a.b, 0, blueLerp);
+    a.r = lerp(a.r, 0, blackLerp);
+    a.g = lerp(a.g, 0, blackLerp);
+    a.b = lerp(a.b, 0, blackLerp);
+    a = adjustLevels(0, 60, 150, { r: a.r, g: a.g, b: a.b });
+    return color(255, 255, 255);
+};
+
+scene = sunset01;
