5a3d4db2df4350266f6e2e0d27401a4f971fdea6
Guillaume Pelletier-Auger <pelletierauger@gmail.com>
2018-07-08 01:39:07 -0400

    Decide on the maximum duration of all scenes

--- scenes-genetic-01.js
+++ scenes-genetic-01.js
@@ -671,17 +671,17 @@ geneticScene05.applyShapes = function() {
 
 geneticScene05.updateGrid = function() {
 
-    if (this.currentState >= this.newListOfRules[this.rulesIndex].d) {
-        // this.mutateRules();
-        if (this.rulesIndex < this.newListOfRules.length) {
-            this.rulesIndex++;
-            this.fileName = this.newListOfRules[this.rulesIndex].name;
-            this.geneticRules = this.newListOfRules[this.rulesIndex].r;
-            this.applyShapes();
-            this.currentState = 0;
-            frameToExport = 1;
-        }
-    }
+    // if (this.currentState >= this.newListOfRules[this.rulesIndex].d) {
+    //     // this.mutateRules();
+    //     if (this.rulesIndex < this.newListOfRules.length) {
+    //         this.rulesIndex++;
+    //         this.fileName = this.newListOfRules[this.rulesIndex].name;
+    //         this.geneticRules = this.newListOfRules[this.rulesIndex].r;
+    //         this.applyShapes();
+    //         this.currentState = 0;
+    //         frameToExport = 1;
+    //     }
+    // }
 
     let xAmount = (this.fixedGridSize) ? this.fixedGridSize.width : this.gridXAmount;
     let yAmount = (this.fixedGridSize) ? this.fixedGridSize.height : this.gridYAmount;
@@ -1068,91 +1068,101 @@ geneticScene05.geneticRules = "0111100010000001";
 geneticScene05.geneticRules = "0100011110000100";
 
 geneticScene05.newListOfRules = [
+
+
+    // { name: "emperess", r: "0110100010000001", d: 425 },
+
     // Sofia
     // "0111100010000001",
-    { name: "sofia-kovalevskaya", r: "0111100010000001", d: 25 },
+    { name: "sofia-kovalevskaya", r: "0111100010000001", d: 350 },
+    { name: "golden-ship", r: "0011100110011001", d: 800 },
+    { name: "big-c", r: "0101111111111101", d: 200 },
+    { name: "sunflowers", r: "0011000010001001", d: 1200 },
+    { name: "feely", r: "0011000010001101", d: 540 },
+    { name: "emperess", r: "0110100000000001", d: 425 },
+    { name: "rocks-in-a-pond", r: "0101111001100001", d: 400 },
     // Ska Shimmer
     // "0111100010000000",
     // Downward landscape
-    { name: "downward-landscape", r: "0110000010000101", d: 200 },
+    { name: "downward-landscape", r: "0110000010000101", d: 400 },
     // Sideway Medusa
-    { name: "sideway-medusa", r: "0011100010000001", d: 200 },
+    { name: "sideway-medusa", r: "0011100010000001", d: 500 },
     // Right boat
-    { name: "right-boat", r: "0110100010000101", d: 200 },
+    { name: "right-boat", r: "0110100010000101", d: 325 },
     // Mid Boat
     // "0110100010000111",
     // Big boat
-    { name: "big-boat", r: "0111100110000001", d: 200 },
+    { name: "big-boat", r: "0111100110000001", d: 350 },
     // Sideway boat
-    { name: "sideway-boat", r: "0101100010000011", d: 200 },
+    { name: "sideway-boat", r: "0101100010000011", d: 450 },
     // 45 Serpinski
-    { name: "45-serpinski", r: "0111110010000000", d: 200 },
+    { name: "45-serpinski", r: "0111110010000000", d: 700 },
     // Mangled 45 Serpinski
-    { name: "mangled-45-serpinski", r: "0111110010010000", d: 200 },
+    { name: "mangled-45-serpinski", r: "0111110010010000", d: 380 },
     // Mangled More
-    { name: "mangled-more", r: "0111110010010010", d: 200 },
+    // I don't like this one... { name: "mangled-more", r: "0111110010010010", d: 200 },
     // Mast
-    { name: "mast", r: "0010010010010011", d: 200 },
+    { name: "mast", r: "0010010010010011", d: 330 },
     // Sideway Pine Tree
-    { name: "sideway-pine-tree", r: "0010010011000001", d: 200 },
+    { name: "sideway-pine-tree", r: "0010010011000001", d: 360 },
     // Towards serpinski
     // "0010010010000000",
     // Rotten Mast
-    { name: "rotten-mast", r: "0010010010000001", d: 200 },
+    { name: "rotten-mast", r: "0010010010000001", d: 360 },
     // Horizontal Serpinski
-    { name: "horizontal-serpinski", r: "0010010011000000", d: 200 },
+    { name: "horizontal-serpinski", r: "0010010011000000", d: 300 },
     // Surprise Serpinski
-    { name: "surprise-serpinski", r: "0010011011000000", d: 200 },
+    { name: "surprise-serpinski", r: "0010011011000000", d: 129 },
     // Facing Serpinski
-    { name: "facing-serpinski", r: "0011011010000000", d: 200 },
+    { name: "facing-serpinski", r: "0011011010000000", d: 260 },
     // Forest Serpinski
-    { name: "forest-serpinski", r: "0010011010000011", d: 200 },
+    { name: "forest-serpinski", r: "0010011010000011", d: 350 },
     // Mossy Serpinski
-    { name: "mossy-serpinski", r: "0001011010010001", d: 200 },
+    { name: "mossy-serpinski", r: "0001011010010001", d: 250 },
     // Calculated Mossy
-    { name: "calculated-mossy", r: "0001011010011111", d: 200 },
+    { name: "calculated-mossy", r: "0001011010011111", d: 305 },
     // "0001011000011111",
     // Bauhaus Descending:
-    { name: "bauhaus-descending", r: "0110100011000101", d: 200 },
+    { name: "bauhaus-descending", r: "0110100011000101", d: 400 },
     // The New Rulers
-    { name: "the-new-rulers", r: "0111100001010101", d: 200 },
+    { name: "the-new-rulers", r: "0111100001010101", d: 350 },
     // Downward Serpinski
     // "0111100001011100",
     // The Boring One
-    { name: "the-boring-one", r: "0001001001111111", d: 200 },
-    { name: "the-boring-two", r: "0001001001111110", d: 200 },
-    { name: "the-boring-three", r: "0101001100111010", d: 200 },
-    { name: "the-boring-four", r: "0101001100110010", d: 200 },
+    // Don't use this one { name: "the-boring-one", r: "0001001001111111", d: 200 },
+    // Don't use this one { name: "the-boring-two", r: "0001001001111110", d: 200 },
+    { name: "the-boring-three", r: "0101001100111010", d: 385 },
+    { name: "the-boring-four", r: "0101001100110010", d: 300 },
     // Shining and Glorious Serpinski
-    { name: "shining-and-glorious-serpinski", r: "0101001100110000", d: 200 },
+    { name: "shining-and-glorious-serpinski", r: "0101001100110000", d: 600 },
     // The Pool Tables of Rural Canada
-    { name: "the-pool-tables-of-rural-canada", r: "0101111111110000", d: 200 },
+    { name: "the-pool-tables-of-rural-canada", r: "0101111111110000", d: 450 },
     // Cranky Serpinski
     // "0100001100110000",
     // Cranky but Growing
-    { name: "cranky-but-growing", r: "0101101110100001", d: 200 },
+    { name: "cranky-but-growing", r: "0101101110100001", d: 250 },
     // Frantic Pinewood Forest
-    { name: "frantic-pinewood-forest", r: "0101001010100001", d: 200 },
+    { name: "frantic-pinewood-forest", r: "0101001010100001", d: 880 },
     // Serene Landscape
-    { name: "serene-landscape", r: "0101001110000001", d: 200 },
+    { name: "serene-landscape", r: "0101001110000001", d: 400 },
     // Broken Landscape
-    { name: "broken-landscape", r: "0101101110000001", d: 200 },
+    { name: "broken-landscape", r: "0101101110000001", d: 400 },
     // Good Growth
-    { name: "good-growth", r: "0100011010100101", d: 200 },
+    { name: "good-growth", r: "0100011010100101", d: 300 },
     // Rainy Two
-    { name: "rainy-two", r: "0110011001101001", d: 200 },
+    { name: "rainy-two", r: "0110011001101001", d: 220 },
     // Inner Fractal
-    { name: "inner-fractal", r: "0000111011100001", d: 200 },
+    { name: "inner-fractal", r: "0000111011100001", d: 375 },
     // Self-Similar Ruler
-    { name: "self-similar-ruler", r: "0001111111111001", d: 200 },
+    { name: "self-similar-ruler", r: "0001111111111001", d: 250 },
     // Between Rain and Maze
-    { name: "between rain and maze", r: "0110011011111100", d: 200 },
+    // Don't use this one { name: "between rain and maze", r: "0110011011111100", d: 200 },
     // Mazes upon Mazes
-    { name: "mazes-upon-mazes", r: "0101111111111100", d: 200 },
+    { name: "mazes-upon-mazes", r: "0101111111111100", d: 600 },
     // Between Mazes and Perfect Box
-    { name: "between-mazes-and-perfect-box", r: "0101111111110110", d: 200 },
+    // Don't use this one { name: "between-mazes-and-perfect-box", r: "0101111111110110", d: 200 },
     // Perfect Box
-    { name: "perfect-box", r: "0110111111110110", d: 200 }
+    // Don't use this one { name: "perfect-box", r: "0110111111110110", d: 200 }
 ];
 
 geneticScene05.listOfRules = [
