2314ff989966d6a7f170ed9120665433046f324b
Guillaume Pelletier-Auger <pelletierauger@gmail.com>
2018-07-28 14:13:22 -0400

    Add the image sequences for the credits in the ffmpeg-input-maker

--- ffmpeg-commands/ffmpeg-input-maker-4.js
+++ ffmpeg-commands/ffmpeg-input-maker-4.js
@@ -8,10 +8,48 @@ let sequences = [{
         copies: 1
     },
     {
-        path: "/Volumes/WD001/genetic-render/excellent-drips-one-fadeout-2/excellent-drips-one-fadeout-2-",
+        path: "/Volumes/WD001/genetic-render/old-render/excellent-drips-one-fadeout-2/excellent-drips-one-fadeout-2-",
         start: 1,
         end: 1188,
         copies: 1
+    },
+
+    // Credit sequence
+    {
+        path: "/Volumes/WD001/genetic-render/rainy-two/rainy-two-",
+        start: 1,
+        end: 1,
+        copies: 12 * 3
+    },
+    {
+        path: "/Volumes/WD001/genetic-render/credits-study-two/1-title-",
+        start: 1,
+        end: 1,
+        copies: 60
+    },
+    {
+        path: "/Volumes/WD001/genetic-render/credits-study-two/2-credits-",
+        start: 1,
+        end: 1,
+        copies: 48
+    },
+    {
+        path: "/Volumes/WD001/genetic-render/credits-study-two/3-calq-",
+        start: 1,
+        end: 1,
+        copies: 72
+    },
+    {
+        path: "/Volumes/WD001/genetic-render/credits-study-two/4-montreal-",
+        start: 1,
+        end: 1,
+        copies: 36
+    },
+    {
+        path: "/Volumes/WD001/genetic-render/rainy-two/rainy-two-",
+        start: 1,
+        end: 1,
+        copies: 18
     }
 ];
 
@@ -28,7 +66,7 @@ for (s of sequences) {
     }
 }
 
-fs.writeFile('./ffmpeg-commands/study-no-2.txt', computedInput, function(err) {
+fs.writeFile('./ffmpeg-commands/study-no-2-credits.txt', computedInput, function(err) {
     if (err) {
         return console.error(err);
     } else {
